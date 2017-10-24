6a00811439387de1870cf20aae3938d82c4fa89c
Jordan Hudgens <jed.flynn.09@gmail.com>
2017-10-24 16:19:37 +0000

    Implemented custom scope for Posts by users

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   enum status: { submitted: 0, approved: 1, rejcted: 2 }
   belongs_to :user
   validates_presence_of :date, :rationale
+  
+  scope :posts_by, ->(user) { where(user_id: user.id)}
 end
